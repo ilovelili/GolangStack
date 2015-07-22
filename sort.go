89591d674747793426eb9ad0b15ce8a5c76a83a1
ilovelili <route666@live.cn>
2015-07-22 11:23:36 +0900

    sort

---
+++ sort.go
@@ -0,0 +1,27 @@
+type sortRunes []rune
+
+func (s sortRunes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s sortRunes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s sortRunes) Len() int {
+    return len(s)
+}
+
+func SortString(s string) string {
+     r := []rune(s)
+	sort.Sort(sortRunes(r))
+	return string(r)
+}	
+
+			func main() {
+				    w1 := "bcad"
+				        w2 := SortString(w1)
+
+					    fmt.Println(w1)
+					        fmt.Println(w2)
+					}
